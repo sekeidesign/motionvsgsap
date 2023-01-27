a635fd18f49d67e713cfcf77dfe35d51182a36be
Piergiorgio Gonni <pg@sekei.ca>
2023-01-27 09:17:11 -0500

    Add home page with links

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-screen h-screen flex flex-col gap-2 items-center justify-center">
+	<h1 class="text-3xl">Testing out two different JavaScript animation libraries:</h1>
+	<span class="text-xl text-slate-500"
+		><a href="/motion" class="text-blue-700 underline hover:decoration-wavy">Motion One</a> and
+		<a href="/gsap" class="text-blue-700 underline hover:decoration-wavy">GSAP</a></span
+	>
+</div>
